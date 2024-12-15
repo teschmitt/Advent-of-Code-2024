0d37826232f4d126c71b9d38c1b6c42d33cccda8
Thomas Schmitt <t.e.schmitt@posteo.de>
2024-12-15 01:39:03 +0100

    trying a faster version

--- day02/src/bin/day02_part2.rs
+++ day02/src/bin/day02_part2.rs
@@ -1,5 +1,3 @@
-#![feature(iter_map_windows)]
-
 use anyhow::Result;
 use nom::{
     character::complete::{newline, space1},
@@ -8,52 +6,50 @@ use nom::{
 use utils::get_input_file_as_string;
 use utils::get_num;
 
-#[derive(Debug, PartialEq, Eq)]
-enum ReportResult {
-    Safe,
-    Unsafe,
-}
-
-impl From<&Vec<u32>> for ReportResult {
-    fn from(report: &Vec<u32>) -> Self {
-        if report.len() <= 1 {
-            return ReportResult::Safe;
-        }
-
-        let mut asc: Option<bool> = None;
-        let mut skipped = false;
-        let mut idx = 0;
-        let mut next_idx = 1;
-        while next_idx < report.len() {
-            let diff = report[next_idx] as i32 - report[idx] as i32;
-            if diff.abs() > 0 && diff.abs() < 4 {
-                if asc.is_none() || ((diff > 0) == asc.unwrap()) {
-                    asc = Some(diff > 0);
-                    idx = next_idx;
-                    next_idx += 1;
-                    continue;
-                }
-                // change in direction
-                else if !skipped {
-                    skipped = true;
-                    idx -= 1;
-                    continue;
-                } else {
-                    return ReportResult::Unsafe;
-                }
-            }
-            // two elements are equal
-            else if !skipped {
-                skipped = true;
-                next_idx += 1;
-                continue;
-            } else {
-                return ReportResult::Unsafe;
-            }
-        }
-
-        ReportResult::Safe
+fn is_safe(report: &Vec<u32>, skipped: bool, mut asc: Option<bool>) -> bool {
+    // println!("Checking {report:?}");
+    if report.len() <= 1 {
+        return true;
     }
+
+    let mut idx = 0;
+    let r_len = report.len();
+    while idx < r_len - 1 {
+        let diff = report[idx + 1] as i32 - report[idx] as i32;
+
+        if asc.is_some() && asc.unwrap() != (diff > 0) {
+            if !skipped {
+                let mut skipped_res = vec![];
+                for i in idx.saturating_sub(1)..=(idx + 1) {
+                    let mut skipped_report = report.clone();
+                    skipped_report.remove(i);
+                    skipped_res.push(is_safe(&skipped_report, true, None));
+                }
+                return skipped_res.iter().any(|&b| b);
+            } else {
+                return false;
+            }
+        }
+
+        asc = Some(diff > 0);
+
+        if diff.abs() == 0 || diff.abs() > 3 {
+            if !skipped {
+                let mut skipped_res = vec![];
+                for i in idx.saturating_sub(1)..=(idx + 1) {
+                    let mut skipped_report = report.clone();
+                    skipped_report.remove(i);
+                    skipped_res.push(is_safe(&skipped_report, true, asc));
+                }
+                return skipped_res.iter().any(|&b| b);
+            } else {
+                return false;
+            }
+        }
+
+        idx += 1;
+    }
+    true
 }
 
 fn main() -> Result<()> {
@@ -67,10 +63,13 @@ fn main() -> Result<()> {
     // let's be lazy and simply iterate over the index if the ReportResult is Unsafe
     let mut res = 0;
     for report in &get_all_reports(&data).unwrap().1 {
-        let r_res: ReportResult = report.into();
+        let r_res = is_safe(report, false, None);
         // println!("{report:?} -> {r_res:?}");
-        if r_res == ReportResult::Safe {
+        if r_res {
             res += 1;
+            // dbg!(res);
+        } else {
+            println!("{report:?} -> {r_res:?}");
         }
     }
 
